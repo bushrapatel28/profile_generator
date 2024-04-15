f652aa66e04141419f3ce05c48f07ce860539b09
Bushra Patel <bpatel60@yahoo.com>
2024-04-14 20:58:43 -0400

    This will create a quick survey app which asks the user a bunch of questions. It will then generate a profile description for them to use online. Once all questions are answered, this survey app will output a fully formed paragraph for their online profile.

--- survey.js
+++ survey.js
@@ -6,22 +6,22 @@ const rl = readline.createInterface({
 });
 
 rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
-  let profilePara = [];
-  profilePara.push(answer);
+  let answers = [];
+  answers.push(answer);
   rl.question("What's an activity you like doing? ", (answer) => {
-    profilePara.push(answer);
+    answers.push(answer);
     rl.question("What do you listen to while doing that? ", (answer) => {
-      profilePara.push(answer);
+      answers.push(answer);
       rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer) => {
-        profilePara.push(answer);
+        answers.push(answer);
         rl.question("What's your favourite thing to eat for that meal? ", (answer) => {
-          profilePara.push(answer);
+          answers.push(answer);
           rl.question("Which sport is your absolute favourite? ", (answer) => {
-            profilePara.push(answer);
+            answers.push(answer);
             rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer) => {
-              profilePara.push(answer);
+              answers.push(answer);
 
-              console.log(`\nThank you for your valuable feedback ${profilePara[0]}! ${profilePara[1]} sounds like a great activity to do while listening to ${profilePara[2]}. Your favourite meal is ${profilePara[3]} and to eat ${profilePara[4]} for that meal. Your favourite sport is ${profilePara[5]} and you possess an amazing superpower of ${profilePara[6]}!`);
+              console.log(`\nThank you for your valuable feedback ${answers[0]}! ${answers[1]} sounds like a great activity to do while listening to ${answers[2]}. Your favourite meal is ${answers[3]} and to eat ${answers[4]} for that meal. Your favourite sport is ${answers[5]} and you possess an amazing superpower of ${answers[6]}!`);
               rl.close();
             });
           });
